ecbaae1544baba8abcf8c18b3fd334dd284a39fb
Ivan Matejev <ivanmatejev@gmail.com>
2022-10-19 17:20:10 +0200

    Project config

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env:{
+      angular:"https://www.globalsqa.com",
+      demoQA:"https://demoqa.com",
+      internetHero:"https://the-internet.herokuapp.com/"
+    }
   },
+  pageLoadTimeout:60000,
+  viewportHeight:1000,
+  viewportWidth:1400
 });
