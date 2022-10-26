7870d7e1287af8a499f063ffe1d51dd97faddcbb
Ivan Matejev <ivanmatejev@gmail.com>
2022-10-26 15:11:51 +0200

    Tooltips upload

---
+++ cypress/e2e/21-tooltips.cy.js
@@ -0,0 +1,18 @@
+describe('Tooltips', () => {
+
+    beforeEach(() => {
+        cy.visit(`${Cypress.env("demoQA")}/tool-tips`)    
+    });
+    
+    it.skip('Hover the button tooltip', () => {
+        cy.get("#toolTipButton").realHover();
+        cy.get(".tooltip-inner").should("have.text", "You hovered over the button")
+
+    });
+
+    it.skip('Hover the A tag tooltip', () => {
+        cy.contains("a","#Contrary").realHover();
+        cy.get(".tooltip-inner").should("have.text", "You hovered over the Contrary")
+
+    });
+});
